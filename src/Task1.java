c1cd06a9e0021a4c54130bea56b6c49ebdca15ba
Daria Orlova <da.orlova365@gmail.com>
2024-02-17 02:56:50 +0300

    Initial Commit

--- src/Task1.java
+++ src/Task1.java
@@ -1,2 +1,9 @@
 public class Task1 {
+    public static void main(String[] args) {
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+    }
 }
